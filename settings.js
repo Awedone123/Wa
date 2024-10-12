24637245be877a4f49afcd658a40778d6a324c1e
Awedone123 <dewaalfiza996@gmail.com>
2024-10-12 14:47:17 +0700

    Delete settings.js

--- settings.js
+++
@@ -1,7 +0,0 @@
-global.prefa = ['','!','.',',','🐤','🗿']
-
-global.owner = ['6283841442290']
-global.botname = 'Rxhl Bot'
-global.baileys1 = require('@whiskeysockets/baileys') 
-global.sticker1 = "Rxhl Official"
-global.sticker2 = "🌜"
